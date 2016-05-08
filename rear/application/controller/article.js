3242f238fddca1cc32a25ef43a6e338a8166896f
ssping <514079588@qq.com>
2016-05-08 23:12:07 +0800

    article add  and search

--- rear/application/controller/article.js
+++ rear/application/controller/article.js
@@ -4,35 +4,43 @@ var Article = mongoose.model('Article')
 
 
 exports.save = function (req,res){
+	var art = req.query.article;
+	console.log(art,"art.....")
 	if(req.session.user){
+		var user = req.session.user._id;
+		art = JSON.parse(art);
+		art.user = user;
+		// article = new Article({
+		// 	name:"test first",
+		// 	content:"from first test ",
+		// 	user:req.session.user._id
+		// })
+		console.log(art,"art....2")
+		var article = new Article(art);
 
-		console.log("req.session.user :",req.session.user);
-		console.log(req.session.user._id,"_id......")
-		article = new Article({
-			name:"test first",
-			content:"from first test ",
-			user:req.session.user._id
-		})
 		article.save(function(err,article){
 			if(err){console.log(err)}
 			console.log(article,"article.....")
 		})
 	}
-	res.json({save:"run..."});
+	res.json({save:"success"});
 }
 exports.search = function (req,res){
 	console.log("insearch")
-	if (req.session.user) {
+	var user = req.session.user;
+	if (user) {
 		console.log("in")
-		Article.find({name:"test first"})//查找的条件
+		Article.find({})//查找的条件
 		// .populate("user","name",{name:"ssp8"},{limit:5})//这里的是填充显示user对象哪些字段。model,field ,match option
-		.populate("user","name",{name:"ssp7"})//这里的是填充显示user对象哪些字段。
+		.populate("user","name",{name:user.name})//这里的是填充显示user对象哪些字段。
 		.exec(function(err,doc){
 			if (err) {console.log(err)};
 			console.log("exec....");
 			console.log(doc,"doc....")
 			res.json(doc);
 		})
+	}else{
+		res.json({error:"user isn't login"});
 	};
 
 }
